e63f2f20c74340862c4a4023ad0221d29d6d018f
Laurent <laurent.thorigny@orange.fr>
2023-11-21 18:50:21 +0100

    p5-test js a finir

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+function changeImg() {
+	// Sélectionnez l'élément img par son ID (getElementById("image");)
+	const imageElement = document.querySelector(arrow .arrow_left);  
+
+	// Modifiez l'attribut src de l'élément img pour changer l'image
+	imageElement.src = "slide1.jpg";
+
+	// Vous pouvez également modifier d'autres attributs si nécessaire
+	imageElement.alt = "Slide 1";
+}
+
+// Sélectionnez le bouton par sa classe
+const monBouton = document.querySelector('.mon-bouton');
+
+// Ajoutez un gestionnaire d'événement de clic au bouton
+monBouton.addEventListener('click', function() {
+    alert('Bouton cliqué !');
+});
+
+
+/*
+<button onclick="changerDiapo()">Changer de diapo</button>
+
+document.getElementById('leftButton').addEventListener('click', function() {
+    var container = document.getElementById('carouselItems');
+    container.scrollLeft -= 300; // Adjust the value to match your item width
+});
+
+document.getElementById('rightButton').addEventListener('click', function() {
+    var container = document.getElementById('carouselItems');
+    container.scrollLeft += 300; // Adjust the value to match your item width
+});
+*/
