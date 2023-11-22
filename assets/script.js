a9ca7976aa49a8c0268be74ecdd080ed6da4c253
Laurent <laurent.thorigny@orange.fr>
2023-11-22 10:27:02 +0100

    p5-affichage dots

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,50 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides)
+// var
+const arrowLeft = document.querySelector('.arrow_left')
+const arrowRight = document.querySelector('.arrow_right')
+const dots = document.querySelector('.dots')
+console.log(arrowLeft) // infos
+console.log(arrowRight) // infos
+console.log(dots) // infos
 
-
-function changeImg() {
-	// Sélectionnez l'élément img par son ID (getElementById("image");)
-	const imageElement = document.querySelector(arrow .arrow_left);  
-
-	// Modifiez l'attribut src de l'élément img pour changer l'image
-	imageElement.src = "slide1.jpg";
-
-	// Vous pouvez également modifier d'autres attributs si nécessaire
-	imageElement.alt = "Slide 1";
+// dots
+function showDots() {
+	for (let i = 0; i < slides.length; i++) { // repete slides.lenght fois
+		//const element = slides[i];
+		const dot = document.createElement("div") // cré une 'div'
+		dot.classList.add("dot") // ajoute une class 'dot' dans la 'div'
+		dots.appendChild(dot) // ajoute le tout entre le container('div) avec la class "dots"
+	}
 }
+console.log('nbr obj in slides tab: ' + slides.length) // infos
+showDots()  // launch la fonction showDots()
 
-// Sélectionnez le bouton par sa classe
-const monBouton = document.querySelector('.mon-bouton');
+// left-arrow
 
-// Ajoutez un gestionnaire d'événement de clic au bouton
-monBouton.addEventListener('click', function() {
-    alert('Bouton cliqué !');
-});
+// right-arrow
+
+arrowLeft.addEventListener('click', () => changeImg())
+arrowRight.addEventListener('click', () => changeImg())
 
 
-/*
-<button onclick="changerDiapo()">Changer de diapo</button>
-
-document.getElementById('leftButton').addEventListener('click', function() {
-    var container = document.getElementById('carouselItems');
-    container.scrollLeft -= 300; // Adjust the value to match your item width
-});
-
-document.getElementById('rightButton').addEventListener('click', function() {
-    var container = document.getElementById('carouselItems');
-    container.scrollLeft += 300; // Adjust the value to match your item width
-});
-*/
+const changeImg = () => {
+	console.log('click sur arrow')
+}
